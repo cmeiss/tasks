3df891c755c76d67b5f8be555afe81f3fe63bc4a
Cornelia <cmeiss@udel.edu>
2023-02-10 14:38:15 -0500

    Task 3

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import "./App.css";
 
 function App(): JSX.Element {
@@ -6,11 +7,52 @@ function App(): JSX.Element {
         <div className="App">
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript : Cornelia Meiss
+                <h1>Rowing is cool</h1>
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload. Hello World.
             </p>
+            <Container>
+                <Row>
+                    <Col>
+                        <img
+                            src={require("./CAA_2.jpg")}
+                            width="200"
+                            height="125"
+                            alt="a picture of our eight at CAAs."
+                        />
+                        <div
+                            id="rectangle"
+                            style={{
+                                width: "100px",
+                                height: "50px",
+                                backgroundColor: "red"
+                            }}
+                        ></div>
+                    </Col>
+                    <Col>
+                        Reasons for Rowing:
+                        <ul>
+                            <li>You see the sunrise every morning</li>
+                            <li>You hang out with amazing people</li>
+                            <li>Regattas!!!!</li>
+                        </ul>
+                        <div
+                            id="rectangle"
+                            style={{
+                                width: "100px",
+                                height: "50px",
+                                backgroundColor: "red"
+                            }}
+                        ></div>
+                    </Col>
+                </Row>
+            </Container>
+
+            <Button onClick={() => console.log("Hello World!")}>
+                Log Hello World
+            </Button>
         </div>
     );
 }
