51241a232c45bb4a49ac6382fd59ecbe3c2c13ee
Cornelia <cmeiss@udel.edu>
2023-02-14 14:48:17 -0500

    fixed convertYesNo

--- src/functions.ts
+++ src/functions.ts
@@ -48,5 +48,11 @@ export function isQuestion(message: string): boolean {
  * upper or lower case), then return `false`. Otherwise, return `null`.
  */
 export function convertYesNo(word: string): boolean | null {
-    return true;
+    const newword = word.toLowerCase();
+    if (newword === "yes") {
+        return true;
+    } else if (newword === "no") {
+        return false;
+    }
+    return null;
 }
