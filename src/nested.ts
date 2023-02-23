c70b0634824ad49225b6a7054321eda923f86131
Cornelia <cmeiss@udel.edu>
2023-02-23 18:54:29 -0500

    fixed sumPublishedPoints

--- src/nested.ts
+++ src/nested.ts
@@ -95,7 +95,12 @@ export function sumPoints(questions: Question[]): number {
  * Consumes an array of questions and returns the sum total of the PUBLISHED questions.
  */
 export function sumPublishedPoints(questions: Question[]): number {
-    return 0;
+    const publishedOnly = getPublishedQuestions(questions);
+    let sum = 0;
+    if (publishedOnly.length !== 0) {
+        sum = sumPoints(publishedOnly);
+    }
+    return sum;
 }
 
 /***
