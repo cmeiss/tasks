52c5434c57b529cdeba04288c541238d21c81a06
Cornelia <cmeiss@udel.edu>
2023-02-23 18:34:45 -0500

    fixed sumPoints

--- src/nested.ts
+++ src/nested.ts
@@ -82,7 +82,13 @@ export function getNames(questions: Question[]): string[] {
  * Consumes an array of questions and returns the sum total of all their points added together.
  */
 export function sumPoints(questions: Question[]): number {
-    return 0;
+    const points = questions.map(
+        (question: Question): number => question.points
+    );
+    const sum = points.reduce(
+        (sum: number, point: number): number => point + sum
+    );
+    return sum;
 }
 
 /***
