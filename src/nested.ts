8a2a1578676089b0d417d783ddc624809440a430
Cornelia <cmeiss@udel.edu>
2023-02-24 07:32:25 -0500

    fixed sameType

--- src/nested.ts
+++ src/nested.ts
@@ -166,7 +166,14 @@ export function publishAll(questions: Question[]): Question[] {
  * are the same type. They can be any type, as long as they are all the SAME type.
  */
 export function sameType(questions: Question[]): boolean {
-    return false;
+    if (questions.length === 0) {
+        return true;
+    }
+    const fqtype = questions[0].type;
+    const same = questions.every(
+        (question: Question): boolean => question.type === fqtype
+    );
+    return same;
 }
 
 /***
