37476cff06190ef13e32f6c8d9fe516cab9676d1
Cornelia <cmeiss@udel.edu>
2023-03-01 15:21:09 -0500

    added ChangeType Button

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -3,5 +3,24 @@ import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
 
 export function ChangeType(): JSX.Element {
-    return <div>Change Type</div>;
+    const [questType, setQuestType] = useState<QuestionType>(
+        "short_answer_question"
+    );
+    function changeQuestType(): void {
+        setQuestType(
+            questType === "short_answer_question"
+                ? "multiple_choice_question"
+                : "short_answer_question"
+        );
+    }
+    return (
+        <div>
+            <Button onClick={changeQuestType}>Change Type</Button>
+            {questType === "multiple_choice_question" ? (
+                <div>Multiple Choice</div>
+            ) : (
+                <div>Short Answer</div>
+            )}
+        </div>
+    );
 }
