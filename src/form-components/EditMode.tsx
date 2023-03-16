a0e972e27b14f2c54f309df8a4a32edc5bd9c760
Cornelia <cmeiss@udel.edu>
2023-03-16 16:39:52 -0400

    fixed editMode

--- src/form-components/EditMode.tsx
+++ src/form-components/EditMode.tsx
@@ -1,9 +1,121 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
+
+interface EditMode {
+    editMode: boolean;
+    setEditMode: (newMode: boolean) => void;
+}
+interface Name {
+    name: string;
+    setName: (newName: string) => void;
+}
+interface Student {
+    student: boolean;
+    setStudent: (newStudent: boolean) => void;
+}
+
+// function InEdit({ name, setName }: Name): // { student, setStudent }: Student
+// JSX.Element {
+//     return (
+//         <div>
+//             <Form.Group controlId="changeName">
+//                 <Form.Label>Enter your name:</Form.Label>
+//                 <Form.Control
+//                     value={name}
+//                     onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+//                         setName(event.target.value)
+//                     }
+//                 />
+//             </Form.Group>
+//             {/* <Form.Check
+//                 type="checkbox"
+//                 id="is-student-check"
+//                 label="Are you a student?"
+//                 checked={student}
+//                 onChange={() => setStudent(!student)}
+//             /> */}
+//         </div>
+//     );
+// }
+
+function InEditName({ name, setName }: Name): JSX.Element {
+    return (
+        <div>
+            <Form.Group controlId="changeName">
+                <Form.Label>Enter your name:</Form.Label>
+                <Form.Control
+                    value={name}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                        setName(event.target.value)
+                    }
+                />
+            </Form.Group>
+        </div>
+    );
+}
+
+function InEditStudent({ student, setStudent }: Student): JSX.Element {
+    return (
+        <div>
+            <Form.Check
+                type="checkbox"
+                id="is-student-check"
+                label="Are you a student?"
+                checked={student}
+                onChange={() => setStudent(!student)}
+            />
+        </div>
+    );
+}
+
+function NotInEdit({
+    name,
+    student
+}: {
+    name: string;
+    student: boolean;
+    // { student }: { student: boolean }
+}): JSX.Element {
+    return (
+        <div>
+            {name} is {student ? "" : "not"} a student.
+        </div>
+    );
+}
 
 export function EditMode(): JSX.Element {
+    const [editMode, setEditMode] = useState<boolean>(false);
+    const [name, setName] = useState<string>("Your Name");
+    const [student, setStudent] = useState<boolean>(true);
     return (
         <div>
             <h3>Edit Mode</h3>
+            <Form.Check
+                type="switch"
+                id="in-edit-check"
+                label="Edit Mode"
+                checked={editMode}
+                onChange={() => setEditMode(!editMode)}
+            />
+            <div>
+                {editMode ? (
+                    // <InEdit
+                    //     name={name}
+                    //     setName={setName}
+                    //     // student={student}
+                    //     // setStudent={setStudent}
+                    // ></InEdit>
+                    <div>
+                        <InEditName name={name} setName={setName}></InEditName>
+                        <InEditStudent
+                            student={student}
+                            setStudent={setStudent}
+                        ></InEditStudent>
+                    </div>
+                ) : (
+                    <NotInEdit name={name} student={student}></NotInEdit>
+                )}
+            </div>
         </div>
     );
 }
